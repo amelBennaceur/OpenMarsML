b7ec79a3634b7bf73757d2bf8f70936af7c49955
Carlos Gavidia-Calderon <cgavidiac@gmail.com>
2022-04-07 13:48:47 +0100

    Plotting training and testing results

--- temp-forecast.py
+++ temp-forecast.py
@@ -1,3 +1,5 @@
+# Inspired by: https://machinelearningmastery.com/multivariate-time-series-forecasting-lstms-keras/
+#  PLotting reference: https://machinelearningmastery.com/time-series-prediction-lstm-recurrent-neural-networks-python-keras/
 from datetime import datetime
 from math import sqrt
 from typing import Callable, List, Tuple
@@ -26,14 +28,13 @@ def plot_timeseries(dataframe: pd.DataFrame):
     plt.show()
 
 
-def pre_process_data(dataframe: pd.DataFrame, period: str) -> Tuple[MinMaxScaler, pd.DataFrame]:
+def pre_process_data(dataframe: pd.DataFrame, period: str, lag: int) -> Tuple[MinMaxScaler, pd.DataFrame]:
     dataframe = dataframe.resample(period).median()
     min_max_scaler: MinMaxScaler = MinMaxScaler(feature_range=(0, 1))
     print("Before scaling: dataframe.values.shape ", dataframe.values.shape)
     dataframe.info()
     dataframe[dataframe.columns] = min_max_scaler.fit_transform(dataframe.values)
 
-    lag: int = 1
     past_values: pd.DataFrame = dataframe.shift(lag)
     past_values.columns = ["{}(t - 1)".format(column_name) for column_name in past_values.columns]
 
@@ -93,21 +94,22 @@ def start_training(model: Sequential, train_x: np.ndarray, train_y: np.ndarray, 
     plt.show()
 
 
-def evaluate(model: Sequential, min_max_scaler: MinMaxScaler, features: np.ndarray, labels: np.ndarray) -> float:
+def evaluate_and_predict(model: Sequential, min_max_scaler: MinMaxScaler, features: np.ndarray,
+                         labels: np.ndarray) -> Tuple[float, np.ndarray, np.ndarray]:
     predictions: np.ndarray = model.predict(features)
     features: np.ndarray = features.reshape((features.shape[0], features.shape[2]))
     target_column_index: int = 1
 
     features[:, target_column_index] = predictions.flatten()
-    original_data: np.ndarray = min_max_scaler.inverse_transform(features)
-    denormalized_predictions: np.ndarray = original_data[:, target_column_index]
+    original_data: np.ndarray = min_max_scaler.inverse_transform(features.copy())
+    denormalized_predictions: np.ndarray = original_data[:, target_column_index].copy()
 
     features[:, target_column_index] = labels.flatten()
-    original_data = min_max_scaler.inverse_transform(features)
-    denormalized_reals: np.ndarray = original_data[:, target_column_index]
+    original_data = min_max_scaler.inverse_transform(features.copy())
+    denormalized_reals: np.ndarray = original_data[:, target_column_index].copy()
 
     rmse: float = sqrt(mean_squared_error(denormalized_reals, denormalized_predictions))
-    return rmse
+    return rmse, denormalized_predictions, denormalized_reals
 
 
 def load_dataset(training_file: str, testing_file: str) -> pd.DataFrame:
@@ -130,11 +132,20 @@ def load_dataset(training_file: str, testing_file: str) -> pd.DataFrame:
     return pd.concat(dataframes, axis=0)
 
 
+def prepare_prediction_data(reference_dataframe: pd.DataFrame, x_start: int, x_end: int,
+                            predictions: np.ndarray) -> np.ndarray:
+    prediction_data: np.ndarray = np.empty(shape=(len(reference_dataframe), 1))
+    prediction_data[:, :] = np.nan
+    prediction_data[x_start:x_end, :] = predictions.reshape(predictions.shape + (1,))
+
+    return prediction_data
+
+
 def main():
-    dataframe: pd.DataFrame = load_dataset('data/insight_openmars_training_time.csv',
-                                           'data/insight_openmars_test_time.csv')
-    print(dataframe[TRAINING_FLAG_COLUMN].value_counts())
-    # plot_timeseries(dataframe)
+    original_dataframe: pd.DataFrame = load_dataset('data/insight_openmars_training_time.csv',
+                                                    'data/insight_openmars_test_time.csv')
+    print(original_dataframe[TRAINING_FLAG_COLUMN].value_counts())
+    plot_timeseries(original_dataframe)
 
     period: str = "D"
     lstm_neurons: int = 50
@@ -142,17 +153,19 @@ def main():
     loss_function: str = "mae"
     optimiser: str = "adam"
     epochs: int = 50
+
     batch_size: int = 72
+    lag: int = 1
 
     min_max_scaler: MinMaxScaler
-    dataframe: pd.DataFrame
-    min_max_scaler, dataframe = pre_process_data(dataframe, period)
+    scaled_dataframe: pd.DataFrame
+    min_max_scaler, scaled_dataframe = pre_process_data(original_dataframe, period, lag)
 
     train_x: np.ndarray
     train_y: np.ndarray
     test_x: np.ndarray
     test_y: np.ndarray
-    train_x, train_y, test_x, test_y = split_in_train_test(dataframe)
+    train_x, train_y, test_x, test_y = split_in_train_test(scaled_dataframe)
 
     print("train_x.shape", train_x.shape)
     print("train_y.shape", train_y.shape)
@@ -162,11 +175,35 @@ def main():
     model: Sequential = define_model(train_x, lstm_neurons, output_neurons, loss_function, optimiser)
     start_training(model, train_x, train_y, test_x, test_y, epochs, batch_size)
 
-    training_rmse: float = evaluate(model, min_max_scaler, train_x, train_y)
+    training_rmse: float
+    training_predictions: np.ndarray
+    training_denormalized: np.ndarray
+
+    training_rmse, training_predictions, training_denormalized = evaluate_and_predict(model, min_max_scaler, train_x,
+                                                                                      train_y)
     print("Training RMSE: {}".format(training_rmse))
 
-    test_rmse: float = evaluate(model, min_max_scaler, test_x, test_y)
+    test_rmse: float
+    testing_predictions: np.ndarray
+    testing_denormalized: np.ndarray
+    test_rmse, testing_predictions, testing_denormalized = evaluate_and_predict(model, min_max_scaler, test_x, test_y)
     print("Test RMSE: {}".format(test_rmse))
+
+    training_prediction_data: np.ndarray = prepare_prediction_data(scaled_dataframe, 0,
+                                                                   len(training_predictions),
+                                                                   training_predictions)
+    testing_prediction_data: np.ndarray = prepare_prediction_data(scaled_dataframe,
+                                                                  len(training_predictions),
+                                                                  len(training_predictions) + len(
+                                                                      testing_predictions),
+                                                                  testing_predictions)
+
+    line_width: float = 0.3
+    plt.plot(np.concatenate([training_denormalized, testing_denormalized]), color="blue", linewidth=line_width)
+    plt.plot(training_prediction_data, color="green", linewidth=line_width)
+    plt.plot(testing_prediction_data, color="red", linewidth=line_width)
+
+    plt.show()
 
 
 if __name__ == "__main__":
