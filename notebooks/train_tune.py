c925b6f097a096728fdbb0791ff5341b56c8d9eb
sagaruprety <sagaruprety@gmail.com>
2024-04-24 15:26:34 +0000

    new Tide model trained

--- notebooks/train_tune.py
+++ notebooks/train_tune.py
@@ -15,7 +15,7 @@ from darts.metrics import mae, mse, mape, rmse, smape
 from darts.dataprocessing.transformers.scaler import Scaler
 from pytorch_lightning.callbacks import EarlyStopping
 from optuna.integration import PyTorchLightningPruningCallback
-from darts.models import RNNModel, BlockRNNModel, TCNModel, TransformerModel, TCNModel, NBEATSModel
+from darts.models import TiDEModel, BlockRNNModel, TCNModel, TransformerModel, TCNModel, NBEATSModel
 
 sys.path.append('../utils/')
 
@@ -78,12 +78,70 @@ test = scaler.transform(test)
 lr_scheduler_cls = torch.optim.lr_scheduler.ExponentialLR
 models = {}
 
+
+def create_tide_model(trial):
+
+    # select input and output chunk lengths
+    in_len = 84
+    out_len =  12
+    batch_size = trial.suggest_categorical('batch_size', [32, 96, 128, 256])
+
+    # Other hyperparameters
+    num_encoder_layers = trial.suggest_categorical("num_encoder_layers", [1, 2])
+    num_decoder_layers =  trial.suggest_categorical("num_decoder_layers", [1, 2])
+    decoder_output_dim = trial.suggest_categorical("decoder_output_dim",[8, 16])
+    hidden_size = trial.suggest_categorical("hidden_size", [64, 128, 256])
+    temporal_decoder_hidden = trial.suggest_categorical("temporal_decoder_hidden", [16, 32, 64])
+    use_layer_norm = trial.suggest_categorical('use_layer_norm', [True, False])
+    dropout = trial.suggest_categorical("dropout", [0.05, 0.1, 0.15, 0.2, 0.25])
+    lr = trial.suggest_float("lr", 5e-5, 1e-3, log=True)
+    # reproducibility
+    torch.manual_seed(42)
+
+    pruner = PyTorchLightningPruningCallback(trial, monitor="val_loss")
+    early_stopper = EarlyStopping("val_loss", min_delta=0.0008, patience=3, verbose=False)
+    callbacks = [pruner, early_stopper]
+
+    pl_trainer_kwargs = {
+    "gradient_clip_val": 1,
+    "max_epochs": 50,
+    "accelerator": "auto",
+    "callbacks": callbacks,
+    }
+
+    common_model_args = {
+        "optimizer_kwargs": {'lr': lr},
+        "pl_trainer_kwargs": pl_trainer_kwargs,
+        "lr_scheduler_cls": lr_scheduler_cls,
+        "lr_scheduler_kwargs": config.lr_scheduler_kwargs,
+        "likelihood": None,  # use a likelihood for probabilistic forecasts
+        "save_checkpoints": True,  # checkpoint to retrieve the best performing model state,
+        "force_reset": True,
+        "random_state": 42,
+
+    }
+    # build the BlockRNNModel model
+    model = TiDEModel(input_chunk_length= in_len,
+                       output_chunk_length = out_len,
+                       num_encoder_layers = num_encoder_layers,
+                       num_decoder_layers = num_decoder_layers,
+                       decoder_output_dim = decoder_output_dim,
+                       hidden_size = hidden_size,
+                       temporal_decoder_hidden = temporal_decoder_hidden,
+                       use_layer_norm = use_layer_norm,
+                       use_reversible_instance_norm =True,
+                       batch_size = batch_size,
+                       model_name="TiDEModel_84_12_HyperOpt", 
+                       dropout = dropout,
+                       **common_model_args)
+    return model
+
 # %%
 def create_block_rnn_model(trial):
 
     # select input and output chunk lengths
-    in_len = trial.suggest_categorical("input_chunk_length", [12, 24, 36, 48, 96])
-    out_len =  in_len-1
+    in_len = 84
+    out_len =  12
     batch_size = trial.suggest_categorical('batch_size', [32, 96, 128, 256])
 
     # Other hyperparameters
@@ -95,7 +153,7 @@ def create_block_rnn_model(trial):
     torch.manual_seed(42)
 
     pruner = PyTorchLightningPruningCallback(trial, monitor="val_loss")
-    early_stopper = EarlyStopping("val_loss", min_delta=0.0008, patience=2, verbose=False)
+    early_stopper = EarlyStopping("val_loss", min_delta=0.0008, patience=3, verbose=False)
     callbacks = [pruner, early_stopper]
 
     pl_trainer_kwargs = {
@@ -123,70 +181,15 @@ def create_block_rnn_model(trial):
                                 n_rnn_layers = n_rnn_layers,
                                 hidden_dim = hidden_dim,
                                 batch_size = batch_size,
-                                model_name="BlockRNNModel", 
+                                model_name="BlockRNNModel_84_12_HyperOpt", 
                                 dropout = dropout,
                                 **common_model_args)
     return model
 
-def create_rnn_model(trial):
-    # select input and output chunk lengths
-    in_len = trial.suggest_categorical("input_chunk_length", [12, 24, 36, 48, 96])
-    out_len =  in_len-1
-    batch_size = trial.suggest_categorical('batch_size', [32, 96, 128, 256])
-    # Other hyperparameters
-
-    n_rnn_layers = trial.suggest_int("n_rnn_layers", 2, 5)
-    hidden_dim =  trial.suggest_int("hidden_dim", 20, 40)
-    dropout = trial.suggest_categorical("dropout", [0.05, 0.1, 0.15, 0.2, 0.25])
-    lr = trial.suggest_float("lr", 5e-5, 1e-3, log=True)
-    # reproducibility
-    torch.manual_seed(42)
-
-
-    pruner = PyTorchLightningPruningCallback(trial, monitor="val_loss")
-    early_stopper = EarlyStopping("val_loss", min_delta=0.0008, patience=2, verbose=False)
-    callbacks = [pruner, early_stopper]
-
-    # detect if a GPU is available
-    if torch.cuda.is_available():
-        num_workers = 8
-    else:
-        num_workers = 0
-
-    pl_trainer_kwargs = {
-    "gradient_clip_val": 1,
-    "max_epochs": 50,
-    "accelerator": "auto",
-    "callbacks": callbacks,
-    }
-
-    common_model_args = {
-        "optimizer_kwargs": {'lr': lr},
-        "pl_trainer_kwargs": pl_trainer_kwargs,
-        "lr_scheduler_cls": lr_scheduler_cls,
-        "lr_scheduler_kwargs": config.lr_scheduler_kwargs,
-        "likelihood": None,  # use a likelihood for probabilistic forecasts
-        "save_checkpoints": True,  # checkpoint to retrieve the best performing model state,
-        "force_reset": True,
-        "batch_size": batch_size,
-        "random_state": 42,
-
-    }
-
-    # build the BlockRNNModel model
-    model = RNNModel(model = "LSTM",
-                                input_chunk_length= in_len,
-                                output_chunk_length = out_len,
-                                n_rnn_layers = n_rnn_layers,
-                                hidden_dim = hidden_dim,
-                                model_name="RNNModel", 
-                                dropout = dropout,
-                                **common_model_args)
-    return model
 
 def create_tcn_model(trial):
-    in_len = trial.suggest_categorical("input_chunk_length", [12, 24, 36, 48, 96])
-    out_len =  in_len-1
+    in_len = 84
+    out_len =  12
     batch_size = trial.suggest_categorical('batch_size', [32, 96, 128, 256])
     # Other hyperparameters
     kernel_size = trial.suggest_int("kernel_size", 2, 5)
@@ -233,7 +236,7 @@ def create_tcn_model(trial):
     weight_norm = weight_norm,
     kernel_size = kernel_size,
     num_filters = num_filters,
-    model_name = 'TCNModel',
+    model_name = 'TCNModel_84_12_HyperOpt',
     dropout = dropout,
     **common_model_args
     )
@@ -241,8 +244,8 @@ def create_tcn_model(trial):
     return model
 
 def create_transformer_model(trial):
-    in_len = trial.suggest_categorical("input_chunk_length", [12, 24, 36, 48, 96])
-    out_len =  in_len-1
+    in_len = 84
+    out_len =  12
     batch_size = trial.suggest_categorical('batch_size', [32, 96, 128, 256])
 
     # Other hyperparameters
@@ -300,15 +303,51 @@ def create_transformer_model(trial):
     dim_feedforward=dim_feedforward,
     dropout=dropout,
     activation="relu",
-    model_name = 'TransformerModel',
+    model_name = 'TransformerModel_84_12_HyperOpt',
     **common_model_args,
 )
     
     return model
 
+def objective_tide(trial):
+    # select input and output chunk lengths
+    model = create_tide_model(trial)
+        # detect if a GPU is available
+    if torch.cuda.is_available():
+        num_workers = 8
+    else:
+        num_workers = 0
+
+    series = create_series(dataframe)
+
+    # when validating during training, we can use a slightly longer validation
+    # set which also contains the first input_chunk_length time steps
+    model_val_set = val
+
+    # train the model
+    model.fit(
+        series=train,
+        val_series=model_val_set,
+        num_loader_workers=num_workers,
+    )
+
+    # reload best model over course of training
+    models[model.model_name] = TiDEModel.load_from_checkpoint("TiDEModel_84_12_HyperOpt", best=True)
+
+    # Evaluate how good it is on the validation set, using sMAPE
+    preds = model.historical_forecasts(series=val, 
+                                        past_covariates=None,
+                                        future_covariates=None,
+                                        retrain=False,
+                                        verbose=True, 
+                                        forecast_horizon=model.model_params['output_chunk_length']
+                                        )
+    smapes = rmse(val['dust'], preds['dust'], n_jobs=-1, verbose=True)
+    return smapes if smapes != np.nan else float("inf")
+
+
 
 # %%
-VAL_LEN = 512
 
 def objective_tcn(trial):
     # select input and output chunk lengths
@@ -333,7 +372,7 @@ def objective_tcn(trial):
     )
 
     # reload best model over course of training
-    models[model.model_name] = TCNModel.load_from_checkpoint("TCNModel", best=True)
+    models[model.model_name] = TCNModel.load_from_checkpoint("TCNModel_84_12_HyperOpt", best=True)
 
     # Evaluate how good it is on the validation set, using sMAPE
     preds = model.historical_forecasts(series=val, 
@@ -368,46 +407,25 @@ def objective_block_rnn(trial):
     )
 
     # reload best model over course of training
-    model = BlockRNNModel.load_from_checkpoint("BlockRNNModel", best=True)
+    model = BlockRNNModel.load_from_checkpoint("BlockRNNModel_84_12_HyperOpt", best=True)
 
     # Evaluate how good it is on the validation set, using sMAPE
-    preds = model.predict(series=train, n=VAL_LEN)
+    preds = model.historical_forecasts(series=val, 
+                                        past_covariates=None,
+                                        future_covariates=None,
+                                        retrain=False,
+                                        verbose=True, 
+                                        forecast_horizon=model.model_params['output_chunk_length']
+                                        )
     smapes = rmse(val['dust'], preds['dust'], n_jobs=-1, verbose=True)
     smape_val = smapes
 
     return smape_val if smape_val != np.nan else float("inf")
-
-def objective_rnn(trial):
-    model = create_rnn_model(trial)
-    if torch.cuda.is_available():
-        num_workers = 8
-    else:
-        num_workers = 0
-
-    # set which also contains the first input_chunk_length time steps
-    model_val_set = val
-    # train the model
-    model.fit(
-        series=train,
-        val_series=model_val_set,
-        num_loader_workers=num_workers,
-    )
-
-    # reload best model over course of training
-    models[model.model_name] = RNNModel.load_from_checkpoint("RNNModel", best=True)
-
-    # Evaluate how good it is on the validation set, using sMAPE
-    preds = model.predict(series=train, n=VAL_LEN)
-    smapes = rmse(val['dust'], preds['dust'], n_jobs=-1, verbose=True)
-    smape_val = smapes
-
-    return smape_val if smape_val != np.nan else float("inf")
-
 
 def create_nbeats_model(trial):
     # select input and output chunk lengths
-    in_len = trial.suggest_categorical("input_chunk_length", [12, 24, 36, 48, 96])
-    out_len =  in_len-1
+    in_len = 84
+    out_len =  12
     batch_size = trial.suggest_categorical('batch_size', [32, 96, 256])
 
     # Other hyperparameters
@@ -456,7 +474,7 @@ def create_nbeats_model(trial):
         loss_fn=torch.nn.MSELoss(),
         input_chunk_length= in_len,
         output_chunk_length = out_len,
-        model_name = 'NBEATSModel',
+        model_name = 'NBEATSModel_84_12_HyperOpt',
         dropout=dropout,
         **common_model_args,
 
@@ -482,10 +500,16 @@ def objective_nbeats(trial):
     )
 
     # reload best model over course of training
-    models[model.model_name] = NBEATSModel.load_from_checkpoint("NBEATSModel", best=True)
+    models[model.model_name] = NBEATSModel.load_from_checkpoint("NBEATSModel_84_12_HyperOpt", best=True)
 
     # Evaluate how good it is on the validation set, using sMAPE
-    preds = model.predict(series=train, n=VAL_LEN)
+    preds = model.historical_forecasts(series=val, 
+                                        past_covariates=None,
+                                        future_covariates=None,
+                                        retrain=False,
+                                        verbose=True, 
+                                        forecast_horizon=model.model_params['output_chunk_length']
+                                        )
     smapes = rmse(val['dust'], preds['dust'], n_jobs=-1, verbose=True)
     smape_val = np.mean(smapes)
 
@@ -511,7 +535,7 @@ def objective_transformer(trial):
     )
 
     # reload best model over course of training
-    models[model.model_name] = TransformerModel.load_from_checkpoint("TransformerModel", best=True)
+    models[model.model_name] = TransformerModel.load_from_checkpoint("TransformerModel_84_12_HyperOpt", best=True)
 
     # Evaluate how good it is on the validation set, using sMAPE
     preds = model.historical_forecasts(series=val, 
@@ -593,19 +617,22 @@ def logging(model, test, study):
         joblib.dump(study, f'../optuna/study_{model.model_name}.pkl')
         mlflow.log_artifact(f'../optuna/study_{model.model_name}.pkl')
 
-
-
-
 def print_callback(study, trial):
     print(f"Current value: {trial.value}, Current params: {trial.params}")
     print(f"Best value: {study.best_value}, Best params: {study.best_trial.params}")
 
 study = optuna.create_study(direction="minimize")
 
-study.optimize(objective_nbeats, n_trials=10, callbacks=[print_callback])
-best_model = create_nbeats_model(study.best_trial)
+import time
+curr_time = time.time()
+
+study.optimize(objective_tide, n_trials=20, callbacks=[print_callback])
+best_model = create_tide_model(study.best_trial)
 best_model = train_model(best_model, val)
 logging(best_model, test, study)
+tide_time = time.time()
+diff = tide_time - curr_time
+print(f'Tide took {diff} seconds')
 exit()
 
 def print_callback(study, trial):
@@ -614,13 +641,35 @@ def print_callback(study, trial):
 
 study = optuna.create_study(direction="minimize")
 
-study.optimize(objective_tcn, n_trials=10, callbacks=[print_callback])
+import time
+curr_time = time.time()
+
+study.optimize(objective_nbeats, n_trials=20, callbacks=[print_callback])
+best_model = create_nbeats_model(study.best_trial)
+best_model = train_model(best_model, val)
+logging(best_model, test, study)
+
+def print_callback(study, trial):
+    print(f"Current value: {trial.value}, Current params: {trial.params}")
+    print(f"Best value: {study.best_value}, Best params: {study.best_trial.params}")
+
+n_beats_time = time.time()
+diff = n_beats_time - curr_time
+print(f'N_beats took {diff} seconds')
+
+study = optuna.create_study(direction="minimize")
+
+study.optimize(objective_tcn, n_trials=20, callbacks=[print_callback])
 
 # %%
 best_model = create_tcn_model(study.best_trial)
 best_model = train_model(best_model, val)
 logging(best_model, test, study)
 
+tcn_time = time.time()
+diff = tcn_time - n_beats_time
+print(f'TCN took {diff} seconds')
+
 # %%
 
 def print_callback(study, trial):
@@ -629,35 +678,16 @@ def print_callback(study, trial):
 
 study = optuna.create_study(direction="minimize")
 
-study.optimize(objective_block_rnn, n_trials=10, callbacks=[print_callback])
+study.optimize(objective_block_rnn, n_trials=20, callbacks=[print_callback])
 
 # %%
 best_model = create_block_rnn_model(study.best_trial)
 best_model = train_model(best_model, val)
 logging(best_model, test, study)
 # exit()
-
-
-# %%
-
-def print_callback(study, trial):
-    print(f"Current value: {trial.value}, Current params: {trial.params}")
-    print(f"Best value: {study.best_value}, Best params: {study.best_trial.params}")
-
-study = optuna.create_study(direction="minimize")
-
-study.optimize(objective_rnn, n_trials=10, callbacks=[print_callback])
-
-# %%
-best_model = create_rnn_model(study.best_trial)
-best_model = train_model(best_model, val)
-logging(best_model, test, study)
-
-# %%
-
-
-
-
+rnn_time = time.time()
+diff = rnn_time - n_beats_time
+print(f'RNN took {diff} seconds')
 
 # %%
 
@@ -667,14 +697,16 @@ def print_callback(study, trial):
 
 study = optuna.create_study(direction="minimize")
 
-study.optimize(objective_transformer, n_trials=10, callbacks=[print_callback])
+study.optimize(objective_transformer, n_trials=20, callbacks=[print_callback])
 
 # %%
 best_model = create_transformer_model(study.best_trial)
 best_model = train_model(best_model, val)
 logging(best_model, test, study)
+ 
 
-# %%
-
+transformer_time  = time.time()
+diff = transformer_time - rnn_time
+print(f'Trnasformer took {diff} secinds')
 
 
